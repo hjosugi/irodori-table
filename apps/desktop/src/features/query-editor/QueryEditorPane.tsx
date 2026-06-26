c73beb40f227a82019b0312f7bd54c23759af451
hjosugi <h.jo.sugi@gmail.com>
2026-06-26 19:42:13 +0900

    feat: move query run control to editor corner

--- apps/desktop/src/features/query-editor/QueryEditorPane.tsx
+++ apps/desktop/src/features/query-editor/QueryEditorPane.tsx
@@ -188,82 +188,6 @@ export function QueryEditorPane({
               </button>
             </div>
           ) : null}
-          <div className="run-control">
-            <button
-              className="primary-action run-main-button"
-              type="button"
-              title={
-                runShortcutLabel
-                  ? `${runPrimaryLabel} (${runShortcutLabel})`
-                  : runPrimaryLabel
-              }
-              disabled={running}
-              onClick={() => void runQuery()}
-            >
-              <Play size={15} fill="currentColor" />
-              <span>{runPrimaryLabel}</span>
-            </button>
-            <button
-              className="primary-action run-menu-toggle"
-              type="button"
-              title="Run options"
-              aria-label="Run options"
-              aria-haspopup="menu"
-              aria-expanded={runMenuOpen}
-              disabled={running}
-              onClick={() => setRunMenuOpen((open) => !open)}
-            >
-              <ChevronDown size={14} />
-            </button>
-            {runMenuOpen ? (
-              <div className="app-menu-popover run-menu-popover" role="menu">
-                <button
-                  type="button"
-                  role="menuitem"
-                  onClick={() => void runQuery()}
-                >
-                  <span>{runPrimaryLabel}</span>
-                  {runShortcutLabel ? <kbd>{runShortcutLabel}</kbd> : null}
-                </button>
-                <button
-                  type="button"
-                  role="menuitem"
-                  disabled={!hasSelectedEditorSql}
-                  onClick={() => void runSelectionQuery()}
-                >
-                  <span>Run Selection</span>
-                </button>
-                <button
-                  type="button"
-                  role="menuitem"
-                  onClick={() => void runCurrentQuery()}
-                >
-                  <span>Run Current</span>
-                  {runCurrentShortcutLabel ? (
-                    <kbd>{runCurrentShortcutLabel}</kbd>
-                  ) : null}
-                </button>
-                <button
-                  type="button"
-                  role="menuitem"
-                  onClick={() => void runFromStartQuery()}
-                >
-                  <span>Run From Top</span>
-                  {runFromStartShortcutLabel ? (
-                    <kbd>{runFromStartShortcutLabel}</kbd>
-                  ) : null}
-                </button>
-                <button
-                  type="button"
-                  role="menuitem"
-                  onClick={() => void runAllQuery()}
-                >
-                  <span>Run All</span>
-                  {runAllShortcutLabel ? <kbd>{runAllShortcutLabel}</kbd> : null}
-                </button>
-              </div>
-            ) : null}
-          </div>
           <button
             className="icon-button"
             type="button"
@@ -340,6 +264,82 @@ export function QueryEditorPane({
               />
             </div>
           </>
+        ) : null}
+      </div>
+      <div className="run-control editor-floating-run">
+        <button
+          className="primary-action run-main-button"
+          type="button"
+          title={
+            runShortcutLabel
+              ? `${runPrimaryLabel} (${runShortcutLabel})`
+              : runPrimaryLabel
+          }
+          disabled={running}
+          onClick={() => void runQuery()}
+        >
+          <Play size={15} fill="currentColor" />
+          <span>{runPrimaryLabel}</span>
+        </button>
+        <button
+          className="primary-action run-menu-toggle"
+          type="button"
+          title="Run options"
+          aria-label="Run options"
+          aria-haspopup="menu"
+          aria-expanded={runMenuOpen}
+          disabled={running}
+          onClick={() => setRunMenuOpen((open) => !open)}
+        >
+          <ChevronDown size={14} />
+        </button>
+        {runMenuOpen ? (
+          <div className="app-menu-popover run-menu-popover" role="menu">
+            <button
+              type="button"
+              role="menuitem"
+              onClick={() => void runQuery()}
+            >
+              <span>{runPrimaryLabel}</span>
+              {runShortcutLabel ? <kbd>{runShortcutLabel}</kbd> : null}
+            </button>
+            <button
+              type="button"
+              role="menuitem"
+              disabled={!hasSelectedEditorSql}
+              onClick={() => void runSelectionQuery()}
+            >
+              <span>Run Selection</span>
+            </button>
+            <button
+              type="button"
+              role="menuitem"
+              onClick={() => void runCurrentQuery()}
+            >
+              <span>Run Current</span>
+              {runCurrentShortcutLabel ? (
+                <kbd>{runCurrentShortcutLabel}</kbd>
+              ) : null}
+            </button>
+            <button
+              type="button"
+              role="menuitem"
+              onClick={() => void runFromStartQuery()}
+            >
+              <span>Run From Top</span>
+              {runFromStartShortcutLabel ? (
+                <kbd>{runFromStartShortcutLabel}</kbd>
+              ) : null}
+            </button>
+            <button
+              type="button"
+              role="menuitem"
+              onClick={() => void runAllQuery()}
+            >
+              <span>Run All</span>
+              {runAllShortcutLabel ? <kbd>{runAllShortcutLabel}</kbd> : null}
+            </button>
+          </div>
         ) : null}
       </div>
     </section>
